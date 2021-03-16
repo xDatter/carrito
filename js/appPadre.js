1953d611118e1214d158b9e50b57478f2c0ad646
icardenas <demwl_14@hotmail.com>
2021-03-16 19:25:32 -0300

    faltan acceder a la info - lo mas importante

--- js/appPadre.js
+++ js/appPadre.js
@@ -2,7 +2,7 @@ const appPadreCarrito=Vue.createApp({
     data(){
         return{
             active:"",
-            
+            ElementosDif:0,
             datosAlCarrito:[],
             actualCarrito:[],
         }
@@ -12,12 +12,26 @@ const appPadreCarrito=Vue.createApp({
             this.active=="active"? (this.active=""):(this.active="active");
         },
         agregarCarrito(datos){
+            let itemCompleto=[];
             let unidades= datos.cantidad;
             let verificador=datos.codigo;
             let valor=datos.precio;
             let producto=datos.nombre;
             let imagen=datos.url;
-            console.log(imagen+""+producto+" "+unidades+" "+verificador+" "+valor);
+            itemCompleto.push(unidades);
+            itemCompleto.push(verificador);
+            itemCompleto.push(valor);
+            itemCompleto.push(producto);
+            itemCompleto.push(imagen);
+            this.datosAlCarrito.push(itemCompleto);
+            this.actualCarrito.push(this.ElementosDif);
+            this.ElementosDif+=1;
+
+        },
+        resetCarro(){
+            this.ElementosDif=0;
+            this.datosAlCarrito=[];
+            this.actualCarrito=[];
 
         },
         sumarItemIgual(){
